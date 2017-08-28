50473618e7d10e7ef001044820525a5aeccc46db
Mawel Mirabal <mmirabalp@gmail.com>
2017-08-28 14:23:21 -0700

    Added From Mini-Navigation buttons, Form sequential

--- dist/step.js
+++ dist/step.js
@@ -1,8 +1,28 @@
 !function() {
-    function n(n) {
-        n.focus();
+    function t() {
+        n() || e($(a).find(".input:invalid").first().parent());
     }
-    $("#contact_form").find(".input").on("change", function(t) {
-        n($(t.target).next());
+    function n() {
+        return document.querySelector(a).checkValidity();
+    }
+    function e(t) {
+        $(".step.active").removeClass("active"), t.addClass("active"), t.find(".input").focus();
+        var n = 2 * t.index(".step") + 1;
+        i($(".path-step:nth-child(" + n + ")"));
+    }
+    function i(t) {
+        $(".path-step.active").removeClass("active"), t.addClass("active");
+    }
+    console.log($(".step.active").index());
+    var a = "#contact_form";
+    $(".path-step").on("click", function(t) {
+        var n = $(t.target);
+        i(n);
+        var a = n.index(".path-step") + 1;
+        e($(".step:nth-child(" + a + ")"));
+    }), $(a).find(".input").on("change", function(n) {
+        var i = $(n.target).parent().next(".step");
+        e(i), i.length > 0 ? (e(i), console.log(" form is  valid")) : (console.log("Error form is not valid"), 
+        t());
     });
 }();
--- js/steps.js
+++ js/steps.js
@@ -1,12 +1,50 @@
 ;(function(){
+
+	// $(".step:ntg-child(1)").addClass("active")
+
+	console.log($(".step.active").index());
+
 	var selector = "#contact_form"
+
+	$(".path-step").on("click", function(event){
+		var $current_circle = $(event.target)
+
+			// $(".path-step.active").removeClass("active")
+			// $current_circle.addClass("active")
+
+			focus_cricle($current_circle);
+
+
+			var position = $current_circle.index(".path-step") + 1;
+			var $test = $(".step:nth-child("+position+")")
+			
+			form_newStep_focus($test)
+
+
+			// console.log(position);
+
+	})
+
+
+
 
 	$(selector).find(".input").on("change", function(event){
 		var $input = $(event.target)
 
-		var $next_input = $input.next()
-		form_newStep_focus($next_input)
+		var $next_step = $input.parent().next(".step")
+		form_newStep_focus($next_step)
+
+
+		if($next_step.length > 0){
+			form_newStep_focus($next_step)
+			console.log(' form is  valid')
+
+		}else{
+			console.log('Error form is not valid')
+			form_validator()
+		}
 		
+
 	})
 
 
@@ -14,15 +52,58 @@
 	// Helpers
 	function form_validator(){
 
+		if(form_is_valid()){
+
+		}else{
+			var $fieldset_invalid = $(selector).find(".input:invalid").first().parent()
+			form_newStep_focus($fieldset_invalid)
+		}
+
+
 	}
 
 	function form_is_valid(){
+		return document.querySelector(selector).checkValidity()
 
 	}
 
-	function form_newStep_focus($next_input){
-		$next_input.focus();
+	function form_newStep_focus($next_step){
+		$(".step.active").removeClass("active")
+		$next_step.addClass("active")
+		$next_step.find(".input").focus()
+
+
+		//COORDINATE CIRCLES IN FORM MINI-NAVEGATION
+		var position = ($next_step.index(".step") *2) +1;
+
+		// $(".path-step.active").removeClass("active")
+
+		var $circle = $(".path-step:nth-child("+position+")")
+		focus_cricle($circle)
+		
+		// $next_step.focus();
 	}
+
+
+
+
+
+
+
+
+
+	function focus_cricle($circle){
+
+					$(".path-step.active").removeClass("active")
+					$circle.addClass("active")
+
+
+			// $(".path-step.active").removeClass("active")
+			// $current_circle.addClass("active")
+
+	}
+
+
 
 })()
 
