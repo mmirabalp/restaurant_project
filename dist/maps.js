309b117fd6372751e90ccb3b36258217750d1b96
Mawel Mirabal <mmirabalp@gmail.com>
2017-08-31 08:42:25 -0700

    Added CSS for the Map, Obtain user Location, Calculate Distance user-> restaurant

--- dist/maps.js
+++ dist/maps.js
@@ -1,11 +1,63 @@
-;(function() {
+"use strict";
+
+function _classCallCheck(e, n) {
+    if (!(e instanceof n)) throw new TypeError("Cannot call a class as a function");
+}
+
+var _createClass = function() {
+    function e(e, n) {
+        for (var o = 0; o < n.length; o++) {
+            var t = n[o];
+            t.enumerable = t.enumerable || !1, t.configurable = !0, "value" in t && (t.writable = !0), 
+            Object.defineProperty(e, t.key, t);
+        }
+    }
+    return function(n, o, t) {
+        return o && e(n.prototype, o), t && e(n, t), n;
+    };
+}();
+
+!function() {
+    var e = function() {
+        function e() {
+            _classCallCheck(this, e);
+        }
+        return _createClass(e, null, [ {
+            key: "get",
+            value: function(e) {
+                return navigator.geolocation ? navigator.geolocation.getCurrentPosition(function(n) {
+                    e({
+                        lat: n.coords.latitude,
+                        lng: n.coords.longitude
+                    });
+                }) : alert("We could not get your location"), {
+                    lat: 0,
+                    lng: 0
+                };
+            }
+        } ]), e;
+    }(), n = {
+        lat: 36.159537,
+        lng: -115.208751
+    };
     google.maps.event.addDomListener(window, "load", function() {
         new google.maps.Map(document.getElementById("map"), {
-            center: {
-                lat: 36.159254,
-                lng: -115.209074
-            },
+            center: n,
             zoom: 15
         });
+        e.get(function(e) {
+            console.log(e), alert("I got User coordenate");
+            var o = new google.maps.LatLng(e.lat, e.lng), t = new google.maps.LatLng(n.lat, n.lng);
+            new google.maps.DistanceMatrixService().getDistanceMatrix({
+                origins: [ o ],
+                destinations: [ t ],
+                travelMode: google.maps.TravelMode.DRIVING
+            }, function(e, n) {
+                if (n === google.maps.DistanceMatrixStatus.OK) {
+                    var o = e.rows[0].elements[0], t = o.duration.text;
+                    console.log(t), console.log(o);
+                }
+            });
+        });
     });
-})();
+}();
--- js/maps.js
+++ js/maps.js
@@ -1,15 +1,82 @@
+
+
 ;(function(){
+
+	class UserLocation{
+
+		static get(callback){
+
+			if(navigator.geolocation){
+			 navigator.geolocation.getCurrentPosition (function(location){
+			 	callback({
+			 		lat: location.coords.latitude,
+			 		lng: location.coords.longitude
+			 	})
+			 }
+			 )
+			}else{
+				alert("We could not get your location")
+			}
+
+			return{
+				lat: 0,
+				lng: 0
+			}
+
+		}
+
+	}
+
+var my_place = {
+
+						lat: 36.159537,
+						lng: -115.208751
+					
+
+}
+
 	google.maps.event.addDomListener(window,"load",function(){
 		var map = new google.maps.Map(document.getElementById('map'),
 				{
-					center:{
-						lat: 19.4248097,
-						lng: -99.19492559999998
-					},
+					center: my_place,
 					zoom: 15
 
 				}
 
 			)
+		UserLocation.get(function(coords){
+			console.log(coords);
+			alert("I got User coordenate")
+
+			// Calculater distanse between restaurant and client location
+			var origin = new google.maps.LatLng(coords.lat, coords.lng)
+			var destiny = new google.maps.LatLng(my_place.lat, my_place.lng)
+
+			var service = new google.maps.DistanceMatrixService()
+			service.getDistanceMatrix({
+				origins: [origin],
+				destinations: [destiny],
+				travelMode: google.maps.TravelMode.DRIVING
+
+
+			},function(response, status){
+				if(status === google.maps.DistanceMatrixStatus.OK){
+					var duration_element = response.rows[0].elements[0]
+					var duration_trip = duration_element.duration.text
+
+					console.log(duration_trip);
+					console.log(duration_element);
+				}
+
+			})
+
+		})
+
+
+
+
 	})
 })();
+
+
+
